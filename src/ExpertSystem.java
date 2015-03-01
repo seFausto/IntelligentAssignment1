c1ee1743fe0ff0bb0c28df4fd5f67e4599646f77
Fausto Ruvalcaba <se.fausto@gmail.com>
2015-02-28 18:38:54 -0800

    changed to JRE 1.7

--- src/ExpertSystem.java
+++ src/ExpertSystem.java
@@ -16,46 +16,46 @@ public class ExpertSystem {
 		int[] initial = new int[2];
 		int numberOfObstacles = 0;
 		String orientation = "Down";
-		
+
 		try {
 			// Expected format:
 			// first value M size of grid
 			String[] gridMN = args[0].split(",");
 			gridM = Integer.parseInt(gridMN[0]);
 			gridN = Integer.parseInt(gridMN[1]);
-			
+
 			// x,y start position of robot
 			String[] initialXY = args[1].split(",");
 			initial[0] = Integer.parseInt(initialXY[0]);
 			initial[1] = Integer.parseInt(initialXY[1]);
-			
+
 			// x,y;x,y;x,y values of obstacles
 			String[] obstaclesInput = args[2].split("\\*");
-			numberOfObstacles= obstaclesInput.length;
-			obstacles = new int [numberOfObstacles][2];
-			
+			numberOfObstacles = obstaclesInput.length;
+			obstacles = new int[numberOfObstacles][2];
+
 			for (int i = 0; i < obstaclesInput.length; i++) {
 				String[] xy = obstaclesInput[i].split(",");
 				obstacles[i][0] = Integer.parseInt(xy[0]);
 				obstacles[i][1] = Integer.parseInt(xy[1]);
-			
+
 			}
-			
+
 			// x,y goal
 			String[] goalXY = args[3].split(",");
 			goal[0] = Integer.parseInt(goalXY[0]);
 			goal[1] = Integer.parseInt(goalXY[1]);
-		
-			//file
+
+			// file
 			outputFile = args[4];
-			
-			//orientation
-			if (args.length >=5)
+
+			// orientation
+			if (args.length > 5)
 				orientation = args[5];
 
 		} catch (Exception e) {
-			System.out.println("Please enter a correct inputs");
-			System.out.println(e.getMessage());
+			System.out.println("Please enter a correct inputs.");
+			System.out.println(e);
 			return;
 		}
 
@@ -63,11 +63,12 @@ public class ExpertSystem {
 		for (int i = 0; i < numberOfObstacles; i++) {
 			world.Grid[obstacles[i][0]][obstacles[i][1]] = Enums.GridValues.O;
 		}
-		
-		Robot robot = new Robot(world, initial[0], initial[1], goal[0], goal[1], orientation);
+
+		Robot robot = new Robot(world, initial[0], initial[1], goal[0],
+				goal[1], orientation);
 
 		robot.MoveTowardsGoal();
-		
+
 		System.out.println(robot._personalGrid.toString());
 		System.out.println("A File has been created: " + outputFile);
 		File.WriteFile(outputFile, robot._personalGrid.toString());
